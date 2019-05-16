9ab87eb4a6ac00f63450ce75d0aaf2f9e8c1dad1
Unknown <danielcatu@gmail.com>
2019-05-16 18:40:39 -0500

    Cambios al date selecter

--- components/modal/Schedule.vue
+++ components/modal/Schedule.vue
@@ -1,17 +1,23 @@
 <template>
 	<div :class="[ openModal ? 'is-active' : '', 'modal' ]">
-		<div class="modal-background"></div>
-		<div class="modal-card">
-			<header class="modal-card-head">
+
+ <v-card>
+        <v-card-title
+          class="headline grey lighten-2"
+          primary-title
+        >
 				<p class="modal-card-title">{{ modalTitle }}</p>
 				<button class="delete" aria-label="close" @click="closeModal(false)"></button>
-			</header>
-			<section class="modal-card-body">
-	<v-layout>
-    <v-flex>
-      <v-sheet height="400">
+        </v-card-title>
+
+        <v-card-text>
+           <v-flex>
+      <v-sheet height="400" width="400">
+				<v-date-picker v-model="picker" color="grey lighten-1" v-if="!daycalendar">
+					
+				</v-date-picker>
         <v-calendar
-          color="primary"
+          color="grey lighten-1"
           type="day"
 		  v-model="todayDate"
 		  :now="todaydate"
@@ -37,9 +43,9 @@
         </v-calendar>
       </v-sheet>
     </v-flex>
-  </v-layout>
-			</section>
-		</div>
+		</v-card-text>
+			<v-divider></v-divider>
+		</v-card>
 	</div>
 </template>
 
@@ -50,15 +56,8 @@ export default {
 	data () {
 		return {
 			modalTitle: 'Carrito de reservas',
-			daycalendar: true,
-			todayDate: new Date().toISOString().substr(0, 10),
-			items:['00:00', '00:30', '01:00', '01:30','02:00','02:30','03:00','03:30','04:00',
-			'04:30','05:00','05:30','06:00','06:30','07:00','07:30','08:00','08:30','09:00','09:30',
-			'10:00','10:30','11:00', '11:30','12:00','12:30','13:00','13:30','14:00',
-			'14:30','15:00','15:30','16:00','16:30','17:00','17:30','18:00','18:30','19:00','19:30',
-			'20:00','20:30','21:00','21:30','22:00','22:30','23:00','23:30','24:00'],
-			present:3
-
+			picker: new Date().toISOString().substr(0, 10),
+			daycalendar: true
 		}
 	},
 
